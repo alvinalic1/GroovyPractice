68daa972c6cf340b1de438fa49c5f93f0b75e83d
Alvin Alic <alvin.alic@gmail.com>
2023-12-12 15:08:26 -0600

    Add question 1.2

---
+++ src/Arrays/Two.groovy
@@ -0,0 +1,9 @@
+package Arrays
+
+//Question 1.2
+//Given two strings, write a method to decide if one is a permutation of the other
+
+boolean checkPermutation(String first, String second)
+{
+    
+}
