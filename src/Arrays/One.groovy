c7240600992efb045ace37ee2eaa251d774a88b0
Alvin Alic <alvin.alic@gmail.com>
2023-12-12 14:39:26 -0600

    Did Question 1.1 in CTCI

--- src/Arrays/One.groovy
+++ src/Arrays/One.groovy
@@ -1,4 +1,29 @@
 package Arrays
 
 //Question 1.1 From Cracking the Coding Interview
+//Implement an algorithm to determine if a string has all unique characters
 
+boolean hasUniqueCharacters(String word)
+{
+    Set<Character> letters = new HashSet<>()
+    boolean unique = true
+
+    word.each { current ->
+        char currentCharacter = current
+        if(letters.contains(currentCharacter))
+        {
+            unique = false
+            return
+        }
+        letters.add(currentCharacter)
+    }
+
+    return unique;
+}
+
+assert hasUniqueCharacters("Hello") == false
+assert hasUniqueCharacters("World") == true
+assert hasUniqueCharacters("abcdefghijklmnopqrstuvwxyz") == true
+assert hasUniqueCharacters("") == true
+
+println "All tests passed"
