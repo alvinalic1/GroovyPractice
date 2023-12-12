b0940606584bd70cc3d9701b0bbc72748d0fe6bd
Alvin Alic <alvin.alic@gmail.com>
2023-12-12 15:40:52 -0600

    Finish question 2

--- src/Arrays/Two.groovy
+++ src/Arrays/Two.groovy
@@ -1,9 +1,24 @@
 package Arrays
 
 //Question 1.2
-//Given two strings, write a method to decide if one is a permutation of the other
+//Given two strings, write a method to decide if one is a permutation of the other (assuming it isn't case sensitive)
 
 boolean checkPermutation(String first, String second)
 {
-    
+    char[] word1 = first.collect {it as char}
+
+    char[] word2 = second.collect{it as char}
+
+    Arrays.sort(word1)
+    Arrays.sort(word2)
+
+    return Arrays.equals(word1, word2)
 }
+
+
+assert checkPermutation("top", "pot") == true
+assert checkPermutation("alvin", "vilan") == true
+assert checkPermutation("samed", "dames") == true
+assert checkPermutation("alvin", "ajla") == false
+
+println "Tests Passes"
